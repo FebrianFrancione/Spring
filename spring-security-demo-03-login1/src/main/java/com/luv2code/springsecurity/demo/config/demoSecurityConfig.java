b6d02ca7ea19165e5077768cdf98f50a406c0247
LettuceFoot <febrian.francione@gmail.com>
2021-01-11 21:12:00 -0500

    Spring module 46

---
+++ spring-security-demo-03-login1/src/main/java/com/luv2code/springsecurity/demo/config/demoSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class demoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        //add ours users
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("mary").password("test123").roles("Manager"))
+                .withUser(users.username("susan").password("test123").roles("Admin"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll();
+
+    }
+
+}
