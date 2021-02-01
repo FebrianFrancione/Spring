6b5b84a5fcb49e67763ba9d9b4348d65e6e0af8f
LettuceFoot <febrian.francione@gmail.com>
2021-02-01 16:46:50 -0500

    Spring module 10

--- spring/src/main/java/com/demo/annotations/TennisCoach.java
+++ spring/src/main/java/com/demo/annotations/TennisCoach.java
@@ -2,10 +2,16 @@ package com.demo.annotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 //now uses default bean id
 @Component
+
 public class TennisCoach implements Coach{
 
     @Autowired
@@ -17,6 +23,17 @@ public class TennisCoach implements Coach{
         System.out.println("TennisCoach: Inside default constructor");
     }
 
+    //define init
+    @PostConstruct
+    public void doMyStartup(){
+        System.out.println("TennisCoach: inside init");
+    }
+
+    //define destroy
+    @PreDestroy
+    public void doMyCleanup(){
+        System.out.println("TennisCoach: inside destroy");
+    }
 
 //    @Autowired
 //    public void doSomeStuff(FortuneService theFortuneService){
