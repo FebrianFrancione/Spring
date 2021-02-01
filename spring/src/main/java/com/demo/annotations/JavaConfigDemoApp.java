3d29e70fba37f868b242668bcb4d34ad0584a34a
LettuceFoot <febrian.francione@gmail.com>
2021-02-01 18:37:25 -0500

    Spring module 11

--- spring/src/main/java/com/demo/annotations/JavaConfigDemoApp.java
+++ spring/src/main/java/com/demo/annotations/JavaConfigDemoApp.java
@@ -1,11 +1,13 @@
 package com.demo.annotations;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AnnotationDemoApp {
+public class JavaConfigDemoApp {
     public static void main(String[] args) {
-        // read spring file config
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("AppCont2.xml");
+        // read spring file java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
         //get bean from spring container default bean
         Coach coach =  context.getBean("tennisCoach", Coach.class);
         //call method on the bean
---
+++ spring/src/main/java/com/demo/annotations/SportConfig.java
@@ -0,0 +1,23 @@
+package com.demo.annotations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+//@ComponentScan
+@PropertySource("sport.properties")
+public class SportConfig {
+    //define bean for sad fortune service
+    @Bean
+    public FortuneService sadFortuneService(){
+        return new SadFortuneService();
+    }
+
+    //define bean for our swim coach and INJECT dependency
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach(sadFortuneService());
+    }
+}
