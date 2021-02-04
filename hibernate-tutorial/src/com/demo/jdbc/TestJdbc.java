0a4c998a0b1d83ef82d0ec45122200861ed8e9eb
LettuceFoot <febrian.francione@gmail.com>
2021-02-04 18:38:30 -0500

    Spring module 19

---
+++ hibernate-tutorial/src/com/demo/jdbc/TestJdbc.java
@@ -0,0 +1,20 @@
+package com.demo.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args) {
+        String jdbUrl = "jdbc:mysql://127.0.0.1:3306/hb_student_tracker?useSSL=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+        try{
+            System.out.println("Connecting to DB" + jdbUrl);
+
+            Connection conn = DriverManager.getConnection(jdbUrl, user, pass);
+            System.out.println("Connection Successful!");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
