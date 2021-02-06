f93297fd562151267aa90a2dcceda1b1a6f5ca34
LettuceFoot <febrian.francione@gmail.com>
2021-02-06 18:20:03 -0500

    Spring module 21 - tested db connection

---
+++ hbOneToOne/src/com/test/hbOne/demo/CreateStudentDemo.java
@@ -0,0 +1,37 @@
+package com.test.hbOne.demo;
+
+import com.demo.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.Configuration;
+
+public class CreateStudentDemo {
+    public static void main(String[] args) {
+        //create session factory
+        SessionFactory factory = new Configuration().configure("hibernate-tutorial/src/hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
+
+        //craet session
+        Session session = factory.getCurrentSession();
+
+        try{
+
+            //use session obj to save JAVA obj
+//           create student obj
+            System.out.println("Creating new Student obj:");
+            Student tempStudent = new Student("Paul", "Wall", "paul@email.com");
+
+            //start transaction
+            session.beginTransaction();
+            //save the student obj
+            System.out.println("saving the Student");
+            session.save(tempStudent);
+
+            //commit trasnaction
+            session.getTransaction().commit();
+            System.out.println("done!");
+        }finally {
+            factory.close();
+        }
+    }
+}
