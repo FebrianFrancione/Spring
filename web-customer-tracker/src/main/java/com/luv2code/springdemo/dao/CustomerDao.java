9e8532f1dea2c4d0b475d79439872c2d7e32216e
LettuceFoot <febrian.francione@gmail.com>
2021-02-17 22:52:58 -0500

    Spring module 29

---
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDao.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo.dao;
+
+import com.luv2code.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDao {
+
+    public List<Customer> getCustomers();
+
+}
