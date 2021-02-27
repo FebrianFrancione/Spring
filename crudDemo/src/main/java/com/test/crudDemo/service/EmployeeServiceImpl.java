b11c01a90e3ac3349d9ccb395ad8efe48a2c3b77
LettuceFoot <febrian.francione@gmail.com>
2021-02-26 20:02:04 -0500

    Spring boot 75 JPA

--- crudDemo/src/main/java/com/test/crudDemo/service/EmployeeServiceImpl.java
+++ crudDemo/src/main/java/com/test/crudDemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.test.crudDemo.service;
 import com.test.crudDemo.Dao.EmployeeDao;
 import com.test.crudDemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,7 +15,7 @@ public class EmployeeServiceImpl implements EmployeeService{
     private EmployeeDao employeeDao;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDao theEmployeeDao){
+    public EmployeeServiceImpl(@Qualifier("employeeDaoJpaImpl") EmployeeDao theEmployeeDao){
         employeeDao = theEmployeeDao;
     }
 
